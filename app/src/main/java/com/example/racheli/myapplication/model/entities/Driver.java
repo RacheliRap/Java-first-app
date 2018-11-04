5abaea92f5b9cbe767228017fc27848a8842940b
Dina Leuchter <dina.leuchter@gmail.com>
2018-11-04 23:02:47 +0200

    added entities

---
+++ app/src/main/java/com/example/racheli/myapplication/model/entities/Driver.java
@@ -0,0 +1,10 @@
+package com.example.racheli.myapplication.model.entities;
+
+public class Driver {
+    private String lastName;
+    private String firstName;
+    private String id;
+    private String phoneNumber;
+    private String email;
+    private String cCardNumber;
+}
---
+++ app/src/main/java/com/example/racheli/myapplication/model/entities/Ride.java
@@ -0,0 +1,14 @@
+package com.example.racheli.myapplication.model.entities;
+
+import java.sql.Time;
+
+public class Ride {
+    private String origin;
+    private String destination;
+    private Time startingTime;
+    private Time endingTime;
+    private String passengerName;
+    private String passengerNumber;
+    private String passengerMail;
+
+}
