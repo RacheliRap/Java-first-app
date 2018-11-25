63432251cef1180c9ef6ac3f2d0be58deeea6a3a
Dina Leuchter <dina.leuchter@gmail.com>
2018-11-25 12:56:10 +0200

    changed backend

---
+++ app/src/main/java/com/example/racheli/myapplication/model/backend/backend.java
@@ -0,0 +1,7 @@
+package com.example.racheli.myapplication.model.backend;
+
+import com.example.racheli.myapplication.model.entities.Ride;
+
+public interface backend {
+    void addRide(Ride myride) throws Exception;
+}
---
+++ app/src/main/java/com/example/racheli/myapplication/model/backend/backendFactory.java
@@ -0,0 +1,4 @@
+package com.example.racheli.myapplication.model.backend;
+
+public class backendFactory {
+}
